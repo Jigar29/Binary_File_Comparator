d18258492cb1847bc5c0e9838e3f5a08a72c5677
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-10-08 13:04:32 -0700

    File Difference Character and Character Position

--- src/file_compare.c
+++ src/file_compare.c
@@ -12,14 +12,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#define FILE_READ_BUFFER_SIZE 1024
 
 int main(void) {
 
 	FILE* file1, *file2;
-
+	long int counter= 1; // to be fixed
 	clock_t start_time, end_time;
-	char *buff1 = (char *)calloc(1025, sizeof(char));
-	char *buff2 = (char *)calloc(1025, sizeof(char));
+	char *buff1 = (char *)calloc(FILE_READ_BUFFER_SIZE+1, sizeof(char));
+	char *buff2 = (char *)calloc(FILE_READ_BUFFER_SIZE+1, sizeof(char));
 
 	file1 = fopen("C:/Users/Jigar/Desktop/file1.txt","r");
 	file2 = fopen("C:/Users/Jigar/Desktop/file2.txt","r");
@@ -28,21 +29,26 @@ int main(void) {
 
 	while((!feof(file1)) && (!feof(file2)))
 	{
-		fgets(buff1,1024, file1);
-		fgets(buff2,1024, file2);
-		if(strcmp(buff1,buff2) != 0)
+		fgets(&buff1[0],FILE_READ_BUFFER_SIZE, file1);
+		fgets(&buff2[0],FILE_READ_BUFFER_SIZE, file2);
+		for(int i =0; i<FILE_READ_BUFFER_SIZE; i++)
 		{
-			printf("Files are not same\n");
-			goto exit;
+			if(buff1[i] != buff2[i])
+			{
+				printf("->Difference file1 = %c%c, file2 = %c%c\n", buff1[i], buff1[i+1], buff2[i], buff2[i+1]);
+				printf("->Files are not same, they defer at %ld position\n", counter+i);
+				goto exit;
+			}
 		}
+		counter = counter + FILE_READ_BUFFER_SIZE;
 	}
-	printf("Files are Exactly same\n");
+	printf("->Files are Exactly same\n");
 
 exit:	fclose(file1);
 		fclose(file2);
 
 	end_time = clock();
-	printf("Total Time taken for the computations is %lf seconds", ((double)(end_time - start_time)/ CLOCKS_PER_SEC));
+	printf("->Total Time taken for the computations is %lf seconds", ((double)(end_time - start_time)/ CLOCKS_PER_SEC));
 	free(buff1);
 	free(buff2);
 	return EXIT_SUCCESS;
