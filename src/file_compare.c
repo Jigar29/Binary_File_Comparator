449bc28bd14d2c9ccc7a408b58d3f55d5974184e
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-10-08 15:18:23 -0700

    No file, End Of File error Checks added

--- src/file_compare.c
+++ src/file_compare.c
@@ -10,23 +10,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint-gcc.h>
 #include <time.h>
 #include <string.h>
+#include <conio.h>
+
+
 #define FILE_READ_BUFFER_SIZE 1024
 
-int main(void) {
+int main( int argc, char *argv[]) {
 
 	FILE* file1, *file2;
-	long int counter= 1; // to be fixed
-	clock_t start_time, end_time;
+	char file1_path[50]= "", file2_path[50]="";
+	int return_status = EXIT_SUCCESS;
+
+	int64_t counter= 1;
+	clock_t start_time;
 	char *buff1 = (char *)calloc(FILE_READ_BUFFER_SIZE+1, sizeof(char));
 	char *buff2 = (char *)calloc(FILE_READ_BUFFER_SIZE+1, sizeof(char));
 
-	file1 = fopen("C:/Users/Jigar/Desktop/file1.txt","r");
-	file2 = fopen("C:/Users/Jigar/Desktop/file2.txt","r");
+	printf("Please Enter the first File\n");
+	scanf("%s", &file1_path[0]);
+	file1 = fopen(&file1_path[0],"r");
+	if(file1 == NULL)
+	{
+		printf("No Such File Exist\n");
+		return_status=  EXIT_FAILURE;
+		goto forced_exit;
+	}
 
+	printf("Please Enter the Second File\n");
+	scanf("%s", &file2_path[0]);
+	printf("\n");
+
+	file2 = fopen(&file2_path[0],"r");
+	if(file2 == NULL)
+	{
+		printf("No Such File Exist\n");
+		fclose(file1);
+		return_status=  EXIT_FAILURE;
+		goto forced_exit;
+	}
 	start_time = clock();
-
+	printf("***************************************************REsult**************************************************\n");
 	while((!feof(file1)) && (!feof(file2)))
 	{
 		fgets(&buff1[0],FILE_READ_BUFFER_SIZE, file1);
@@ -35,21 +61,25 @@ int main(void) {
 		{
 			if(buff1[i] != buff2[i])
 			{
-				printf("->Difference file1 = %c%c, file2 = %c%c\n", buff1[i], buff1[i+1], buff2[i], buff2[i+1]);
-				printf("->Files are not same, they defer at %ld position\n", counter+i);
+				printf("Output : Files are not same, First most difference is at %I64d position\n", counter+i);
+				if((buff1[i] != '\0') && (buff2[i] != '\0'))
+					printf("Reason : First Different character in both the files is \n\t file1 = '%c'%c, file2 = '%c'%c\n", buff1[i], buff1[i+1], buff2[i], buff2[i+1]);
+				else
+					printf("Reason : File Size are not equal.\n");
 				goto exit;
 			}
 		}
 		counter = counter + FILE_READ_BUFFER_SIZE;
 	}
-	printf("->Files are Exactly same\n");
+	printf("Output : Files are Exactly same\n");
 
-exit:	fclose(file1);
-		fclose(file2);
+exit:fclose(file1);
+	 fclose(file2);
+	 free(buff1);
+	 free(buff2);
 
-	end_time = clock();
-	printf("->Total Time taken for the computations is %lf seconds", ((double)(end_time - start_time)/ CLOCKS_PER_SEC));
-	free(buff1);
-	free(buff2);
-	return EXIT_SUCCESS;
+	 printf("\n\nTotal Time taken for the computations is %lf seconds", ((double)(clock() - start_time)/ CLOCKS_PER_SEC));
+
+forced_exit:	 getch();
+	 	 	 	 return return_status;
 }
