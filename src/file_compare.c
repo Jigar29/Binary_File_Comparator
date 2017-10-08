05e9ce76770732adff68198e3cd9857f0ee850c2
Jigar Agrawal <jigaragrawal29@gmail.com>
2017-10-08 12:00:45 -0700

    Commit for test using the string compare

--- src/file_compare.c
+++ src/file_compare.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : file_compare.c
- Author      : 
+ Author      : Jigar
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -10,21 +10,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 int main(void) {
 
 	FILE* file1, *file2;
 
+	clock_t start_time, end_time;
+	char *buff1 = (char *)calloc(1025, sizeof(char));
+	char *buff2 = (char *)calloc(1025, sizeof(char));
+
 	file1 = fopen("C:/Users/Jigar/Desktop/file1.txt","r");
 	file2 = fopen("C:/Users/Jigar/Desktop/file2.txt","r");
 
+	start_time = clock();
+
+	while((!feof(file1)) && (!feof(file2)))
 	{
-		if(fgetc(file1) != fgetc(file2))
+		fgets(buff1,1024, file1);
+		fgets(buff2,1024, file2);
+		if(strcmp(buff1,buff2) != 0)
 		{
 			printf("Files are not same\n");
-			return EXIT_SUCCESS;
+			goto exit;
 		}
-		printf("Files are exacclty same\n");
 	}
+	printf("Files are Exactly same\n");
+
+exit:	fclose(file1);
+		fclose(file2);
+
+	end_time = clock();
+	printf("Total Time taken for the computations is %lf seconds", ((double)(end_time - start_time)/ CLOCKS_PER_SEC));
+	free(buff1);
+	free(buff2);
 	return EXIT_SUCCESS;
 }
